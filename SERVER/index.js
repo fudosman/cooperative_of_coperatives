c582484356e8b2ed7b384f3713c6f5dd532c46a2
afudoh kachi <fudosman@gmail.com>
2021-11-28 16:50:04 +0100

    adding all the router and controler folders

--- SERVER/index.js
+++ SERVER/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const authRoute = require("./routes/auth");
 const dotenv = require("dotenv");
-const postRoute = require("./routes/posts");
-const orders = require("./routes/orders");
-const crud = require("./routes/crud");
+const blogRoute = require("./routes/blogs/blog");
+const coopRoute = require("./routes/coops/coop");
+const eventsRoute = require("./routes/events/event");
+const loansRoute = require("./routes/loans/loan");
+const notificationsRoute = require("./routes/notifications/notification");
+const poolsRoute = require("./routes/pools/pool");
+const usersRoute = require("./routes/users/user");
+const walletsRoute = require("./routes/wallets/wallet");
 const app = express();
 
 dotenv.config();
@@ -12,7 +16,7 @@ dotenv.config();
 mongoose
   .connect(process.env.DB_CONNECT_LOCAL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log("Successfully connected to MongoDB hotelOrders!");
@@ -22,24 +26,33 @@ mongoose
     console.error(error);
   });
 
-  app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
-  
-  // for parsing application/json
-  app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 
-  // for parsing application/x-www-form-urlencoded
-  app.use(express.urlencoded({ extended: true })) ;
+// for parsing application/json
+app.use(express.json());
+
+// for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
 
 // middlewares
-app.use("/api/user", authRoute);
-app.use("/api/posts", postRoute);
-app.use("/api/orders", orders);
-app.use("/api/crud", crud);
-
+app.use("/api/blog", blogRoute);
+app.use("/api/group", coopRoute);
+app.use("/api/user", usersRoute);
+app.use("/api/event", eventsRoute);
+app.use("/api/loan", loansRoute);
+app.use("/api/notification", notificationsRoute);
+app.use("/api/pool", poolsRoute);
+app.use("/api/wallet", walletsRoute);
 
 module.exports = app;
